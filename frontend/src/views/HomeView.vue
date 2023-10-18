62cc79ed51ce7fa136178a58eef14fd509922ea6
MDU <genonchev@gmail.com>
2023-10-18 18:19:19 +0300

    Add Edit Logic

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -8,12 +8,17 @@ import { useModalStore } from '@/stores/modalStore'
 
 const modalStore = useModalStore()
 const modal = useModal();
+
+const openModal = () => {
+  modalStore.toggleModal();
+  modalStore.addRegNumber(0);
+}
 </script>
 
 <template>
   <div id="app" class="flex demo">
     {{ modalStore.regNumber }}
-    <button class="success full" @click="modalStore.toggleModal">Add New</button>
+    <button class="success full" @click="openModal">Add New</button>
 
     <Modal v-model="modalStore.isModalOpen">
       <!-- Your existing form goes here -->
